89933a97d57aa7180384f6164372b34b23ba5b6b
jguzman4 <jguzman23@mail.csuchico.edu>
2017-07-24 09:46:11 -0700

    adding more output for testing scrit

--- testing-rest-api.sh
+++ testing-rest-api.sh
@@ -2,9 +2,11 @@
 endpoint="all"
 curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
 success=200
+echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
-        echo "success... "
+        echo "done."
 else
+        echo "curl command failed."
         exit 1;
 fi
 
@@ -12,8 +14,9 @@ curl=
 endpoint=
 endpoint="name/united"
 curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
-        echo "success... "
+        echo "done."
 else
         echo "curl command failed."
         exit 1;
@@ -23,8 +26,9 @@ curl=
 endpoint=
 endpoint="name/aruba?fullText=true"
 curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
-        echo "success... "
+        echo "done."
 else
         echo "curl command failed."
         exit 1;
@@ -34,8 +38,9 @@ curl=
 endpoint=
 endpoint="alpha/co"
 curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
-        echo "success... "
+        echo "done."
 else
         echo "curl command failed."
         exit 1;
@@ -45,8 +50,9 @@ curl=
 endpoint=
 endpoint="alpha?codes=col;no;ee"
 curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
-        echo "success... "
+        echo "done."
 else
         echo "curl command failed."
         exit 1;
@@ -56,8 +62,9 @@ curl=
 endpoint=
 endpoint="currency/cop"
 curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
-        echo "success... "
+        echo "done."
 else
         echo "curl command failed."
         exit 1;
@@ -67,8 +74,9 @@ curl=
 endpoint=
 endpoint="lang/es"
 curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
-        echo "success... "
+        echo "done."
 else
         echo "curl command failed."
         exit 1;
@@ -78,8 +86,9 @@ curl=
 endpoint=
 endpoint="capital/tallinn"
 curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
-        echo "success... "
+        echo "done."
 else
         echo "curl command failed."
         exit 1;
@@ -89,8 +98,9 @@ curl=
 endpoint=
 endpoint="callingcode/372"
 curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
-        echo "success... "
+        echo "done."
 else
         echo "curl command failed."
         exit 1;
@@ -100,8 +110,9 @@ curl=
 endpoint=
 endpoint="region/europe"
 curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
-        echo "success... "
+        echo "done."
 else
         echo "curl command failed."
         exit 1;
@@ -111,8 +122,9 @@ curl=
 endpoint=
 endpoint="regionalbloc/eu"
 curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
-        echo "success... "
+        echo "done."
 else
         echo "curl command failed."
         exit 1;
