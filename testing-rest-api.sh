f3724fea90198d8c136c3a6ece0e353b465eac73
Nelsonater <alexsnelson95@gmail.com>
2017-07-24 09:54:41 -0700

    Updated api test script to reflect new version of dockerfile

--- testing-rest-api.sh
+++ testing-rest-api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 endpoint="all"
-curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+curl="$(curl -sI http://localhost:8080/restcountries/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
 success=200
 echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
@@ -13,7 +13,7 @@ fi
 curl=
 endpoint=
 endpoint="name/united"
-curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+curl="$(curl -sI http://localhost:8080/restcountries/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
 echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
         echo "done."
@@ -25,7 +25,7 @@ fi
 curl=
 endpoint=
 endpoint="name/aruba?fullText=true"
-curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+curl="$(curl -sI http://localhost:8080/restcountries/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
 echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
         echo "done."
@@ -37,7 +37,7 @@ fi
 curl=
 endpoint=
 endpoint="alpha/co"
-curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+curl="$(curl -sI http://localhost:8080/restcountries/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
 echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
         echo "done."
@@ -49,7 +49,7 @@ fi
 curl=
 endpoint=
 endpoint="alpha?codes=col;no;ee"
-curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+curl="$(curl -sI http://localhost:8080/restcountries/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
 echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
         echo "done."
@@ -61,7 +61,7 @@ fi
 curl=
 endpoint=
 endpoint="currency/cop"
-curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+curl="$(curl -sI http://localhost:8080/restcountries/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
 echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
         echo "done."
@@ -73,7 +73,7 @@ fi
 curl=
 endpoint=
 endpoint="lang/es"
-curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+curl="$(curl -sI http://localhost:8080/restcountries/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
 echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
         echo "done."
@@ -85,7 +85,7 @@ fi
 curl=
 endpoint=
 endpoint="capital/tallinn"
-curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+curl="$(curl -sI http://localhost:8080/restcountries/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
 echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
         echo "done."
@@ -97,7 +97,7 @@ fi
 curl=
 endpoint=
 endpoint="callingcode/372"
-curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+curl="$(curl -sI http://localhost:8080/restcountries/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
 echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
         echo "done."
@@ -109,7 +109,7 @@ fi
 curl=
 endpoint=
 endpoint="region/europe"
-curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+curl="$(curl -sI http://localhost:8080/restcountries/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
 echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
         echo "done."
@@ -121,7 +121,7 @@ fi
 curl=
 endpoint=
 endpoint="regionalbloc/eu"
-curl="$(curl -sI http://localhost:8080/restcountries-2.0.2/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
+curl="$(curl -sI http://localhost:8080/restcountries/rest/v2/"$endpoint" | head -1 | grep -oE '200')"
 echo "checking /rest/v2/$endpoint... "
 if [ "$curl" == "$success" ]; then
         echo "done."
